6b4b1d273f57d53e5c279a331b6d55f8bab9128e
Lucas Gabriel <lucas.abade2104@gmail.com>
2026-03-29 19:53:36 +0000

    Criando classe mago

--- projeto-heranca/src/br/edu/rpg/modelo/Mago.java
+++ projeto-heranca/src/br/edu/rpg/modelo/Mago.java
@@ -1,5 +1,22 @@
 package br.edu.rpg.modelo;
 
-public class Mago {
-    
+public class Mago extends Personagem {
+    private int mana;
+
+    //Construtor
+    public Mago(String nome, int nivel, int mana) {
+        super(nome, nivel);
+        this.mana = mana;
+    }
+
+    //Método Genérico Sobrescrito
+    @Override
+    public void atacar() {
+        System.out.println(getNome() + " realizou um ataque com mana! Causando "+ mana +" de dano!");
+    }
+
+    // Getters e Setters (Encapsulamento)
+    public int getMana() { return mana; }
+    public int setMana(int mana) { this.mana = mana; }
+
 }
