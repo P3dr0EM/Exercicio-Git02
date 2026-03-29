7fb9e68544cb4a8bb0eef038324c47e4f97219b9
Pedro Gabriel Evangelista Marques <pedroka.evangelista@gmail.com>
2026-03-29 16:11:18 -0300

    Superclasse Personagem e construção da Main

---
+++ projeto-heranca/src/br/edu/rpg/Main.java
@@ -0,0 +1,22 @@
+
+package br.edu.rpg;
+import br.edu.rp.modelo.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Polimorfismo: Lista da Superclasse contendo Subclasses
+        List<Personagem> grupo = new ArrayList<>();
+
+        grupo.add(new Guerreiro("Aragorn", 10, 50));
+        grupo.add(new Mago("Gandalf", 12, 100));
+
+        System.out.println("--- Início do Combate ---");
+        for (Personagem p : grupo) {
+            System.out.println(p.toString());
+            p.atacar(); // Chama o método sobrescrito de cada classe (Polimorfismo)
+            System.out.println("-------------------------");
+        }
+    }
+}
